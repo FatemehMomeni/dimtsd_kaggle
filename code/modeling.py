bcdea84d69dbd1e8c729f820fe541a6167f42829
FatemehMomeni <50900356+FatemehMomeni@users.noreply.github.com>
2023-10-11 13:43:56 +0330

    Update modeling.py

--- code/modeling.py
+++ code/modeling.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-from transformers import AutoModel, BertModel
+from transformers import AutoModel, BertModel, BertConfig
 
 
 class stance_classifier(nn.Module):
@@ -9,18 +9,15 @@ class stance_classifier(nn.Module):
         self.dropout = nn.Dropout(0.)
         self.relu = nn.ReLU()        
         if model_select == 'Bertweet':
-            self.bert = AutoModel.from_pretrained("vinai/bertweet-base")
+            configuration = BertConfig(max_position_embeddings=512)            
+            self.bert = AutoModel.from_pretrained("vinai/bertweet-base", configuration)
         elif model_select == 'Bert':
             self.bert = BertModel.from_pretrained("bert-base-uncased")
         self.bert.pooler = None
         self.linear = nn.Linear(self.bert.config.hidden_size, self.bert.config.hidden_size)
         self.out = nn.Linear(self.bert.config.hidden_size, num_labels)
         
-    def forward(self, x_input_ids, x_seg_ids, x_atten_masks, x_len):        
-        print('==='*10)
-        print(x_input_ids.size())
-        print(x_seg_ids.size())
-        print(x_atten_masks.size())
+    def forward(self, x_input_ids, x_seg_ids, x_atten_masks, x_len):                
         last_hidden = self.bert(input_ids=x_input_ids, attention_mask=x_atten_masks, token_type_ids=x_seg_ids)
         
         cls = last_hidden[0][:,0]
