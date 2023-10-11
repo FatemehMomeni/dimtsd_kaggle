9fba98cd9c583ffb6bb358098a29e26aabd46352
FatemehMomeni <50900356+FatemehMomeni@users.noreply.github.com>
2023-10-11 16:23:25 +0330

    Update modeling.py

--- code/modeling.py
+++ code/modeling.py
@@ -9,7 +9,7 @@ class stance_classifier(nn.Module):
         self.dropout = nn.Dropout(0.)
         self.relu = nn.ReLU()        
         if model_select == 'Bertweet':
-            self.bert = AutoModel.from_pretrained("vinai/bertweet-large")
+            self.bert = AutoModel.from_pretrained("vinai/bertweet-base")
         elif model_select == 'Bert':
             self.bert = BertModel.from_pretrained("bert-base-uncased")
         self.bert.pooler = None
