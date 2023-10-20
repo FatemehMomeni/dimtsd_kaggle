ed301a2a7767d4806b297cc55689af24078f80e9
FatemehMomeni <50900356+FatemehMomeni@users.noreply.github.com>
2023-10-20 13:53:13 +0330

    Update modeling.py

--- code/modeling.py
+++ code/modeling.py
@@ -1,27 +1,62 @@
+# --------------------------------------------- indicatpr = 7 ---------------------------------------------------------------
+# import torch
+# import torch.nn as nn
+# from transformers import AutoModel, BertModel
+
+
+# class stance_classifier(nn.Module):
+#     def __init__(self,num_labels,model_select):
+#         super(stance_classifier, self).__init__()        
+#         self.dropout = nn.Dropout(0.)
+#         self.relu = nn.ReLU()        
+#         if model_select == 'Bertweet':
+#             self.bert = AutoModel.from_pretrained("vinai/bertweet-base")
+#         elif model_select == 'Bert':
+#             self.bert = BertModel.from_pretrained("bert-base-uncased")
+#         self.bert.pooler = None
+#         self.linear = nn.Linear(self.bert.config.hidden_size, self.bert.config.hidden_size)
+#         self.out = nn.Linear(self.bert.config.hidden_size, num_labels)
+        
+#     def forward(self, x_input_ids, x_seg_ids, x_atten_masks, x_len):                
+#         last_hidden = self.bert(input_ids=x_input_ids, attention_mask=x_atten_masks, token_type_ids=x_seg_ids)
+        
+#         cls = last_hidden[0][:,0]
+#         query = self.dropout(cls)
+#         linear = self.relu(self.linear(query))
+#         out = self.out(linear)
+        
+#         return out
+
+# -------------------------------------------------------- indicator = 9 --------------------------------------------------------
+
 import torch
 import torch.nn as nn
 from transformers import AutoModel, BertModel
 
 
 class stance_classifier(nn.Module):
-    def __init__(self,num_labels,model_select):
-        super(stance_classifier, self).__init__()        
-        self.dropout = nn.Dropout(0.)
-        self.relu = nn.ReLU()        
-        if model_select == 'Bertweet':
-            self.bert = AutoModel.from_pretrained("vinai/bertweet-base")
-        elif model_select == 'Bert':
-            self.bert = BertModel.from_pretrained("bert-base-uncased")
-        self.bert.pooler = None
-        self.linear = nn.Linear(self.bert.config.hidden_size, self.bert.config.hidden_size)
-        self.out = nn.Linear(self.bert.config.hidden_size, num_labels)
+  def __init__(self,num_labels,model_select):
+    super(stance_classifier, self).__init__()        
+    self.dropout = nn.Dropout(0.)
+    self.relu = nn.ReLU()        
+    if model_select == 'Bertweet':
+      self.bert = AutoModel.from_pretrained("vinai/bertweet-base")
+    elif model_select == 'Bert':
+      self.bert = BertModel.from_pretrained("bert-base-uncased")
+    self.bert.pooler = None
+    self.linear = nn.Linear(self.bert.config.hidden_size, self.bert.config.hidden_size)
+    self.out = nn.Linear(self.bert.config.hidden_size, num_labels)
         
-    def forward(self, x_input_ids, x_seg_ids, x_atten_masks, x_len):                
-        last_hidden = self.bert(input_ids=x_input_ids, attention_mask=x_atten_masks, token_type_ids=x_seg_ids)
-        
-        cls = last_hidden[0][:,0]
-        query = self.dropout(cls)
-        linear = self.relu(self.linear(query))
-        out = self.out(linear)
-        
-        return out
+  def forward(self, x_input_ids, x_seg_ids, x_atten_masks, mask_pos):  
+    last_hidden = self.bert(input_ids=x_input_ids, attention_mask=x_atten_masks, token_type_ids=x_seg_ids)[0]
+    predictions = tuple()
+    for i in range(len(x_input_ids)):
+      predicted_mask_token = last_hidden[i, mask_pos[i]]
+      predictions = predictions + (predicted_mask_token,)
+    prediction_tensor = torch.stack(predictions).cuda()
+    # cls = last_hidden[0][:,0]
+    query = self.dropout(prediction_tensor)
+    linear = self.relu(self.linear(query))
+    out = self.out(linear)
+    
+    return out
