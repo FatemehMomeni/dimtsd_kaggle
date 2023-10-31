b537129ffcb28a5ba4e87ec63b7632101271464f
FatemehMomeni <50900356+FatemehMomeni@users.noreply.github.com>
2023-10-31 19:10:39 +0330

    Update error_analysis_model.py

--- code/error_analysis_model.py
+++ code/error_analysis_model.py
@@ -35,15 +35,15 @@ from transformers import AutoModel, BertModel
 class stance_classifier(nn.Module):
   def __init__(self,num_labels,model_select, label_vectors):
     super(stance_classifier, self).__init__()        
-    self.dropout = nn.Dropout(0.)
-    self.relu = nn.ReLU()        
+    # self.dropout = nn.Dropout(0.)
+    # self.relu = nn.ReLU()        
     if model_select == 'Bertweet':
       self.bert = AutoModel.from_pretrained("vinai/bertweet-base")
     elif model_select == 'Bert':
       self.bert = BertModel.from_pretrained("bert-base-uncased")
-    self.bert.pooler = None
-    self.linear = nn.Linear(self.bert.config.hidden_size, self.bert.config.hidden_size)
-    self.out = nn.Linear(self.bert.config.hidden_size, num_labels)
+    # self.bert.pooler = None
+    # self.linear = nn.Linear(self.bert.config.hidden_size, self.bert.config.hidden_size)
+    # self.out = nn.Linear(self.bert.config.hidden_size, num_labels)
     # self.mask_cls = nn.Linear(self.bert.config.hidden_size, 512)
     self.labels = label_vectors
         
