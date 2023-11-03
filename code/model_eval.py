1a07376b64daadae86f961fcad5187ac19018698
FatemehMomeni <50900356+FatemehMomeni@users.noreply.github.com>
2023-11-03 14:41:17 +0330

    Update model_eval.py

--- code/model_eval.py
+++ code/model_eval.py
@@ -5,8 +5,9 @@ from sklearn.metrics import precision_recall_fscore_support, confusion_matrix
 
 
 # Evaluation
-def compute_f1(preds, y):
+def compute_f1(preds_a, preds_n, preds_f, y, test):
     
+    preds = torch.stack((preds_a, preds_n, preds_f)).cuda()
     rounded_preds = torch.nn.functional.softmax(preds)
     _, indices = torch.max(rounded_preds, 1)
                 
@@ -19,4 +20,8 @@ def compute_f1(preds, y):
 
     f1_average = (result[2][0]+result[2][2])/2  # average F1 score of Favor and Against
         
-    return acc, f1_average, result[0], result[1], confusion_matrix(y_true, y_pred)
+    if test:
+      confusion_matrix = confusion_matrix(y_true, y_pred)
+    else:
+      confusion_matrix = None
+    return acc, f1_average, result[0], result[1], confusion_matrix
