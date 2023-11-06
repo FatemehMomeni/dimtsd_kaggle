62e8eb10f7a975d2a12cba6b2e76be0929914cc9
FatemehMomeni <50900356+FatemehMomeni@users.noreply.github.com>
2023-11-06 13:12:50 +0330

    Update error_analysis_data.py

--- code/error_analysis_data.py
+++ code/error_analysis_data.py
@@ -86,62 +86,65 @@ from torch.utils.data import TensorDataset, DataLoader
 from transformers import BertTokenizer, AutoTokenizer, BertweetTokenizer
     
 
-def convert_data_to_ids(tokenizer, text):    
-  input_ids, attention_masks, seg_ids, mask_pos = [], [], [], []
+def convert_data_to_ids(tokenizer, text, y_str):    
+  input_ids, seg_ids, attention_masks, mask_pos = [], [], [], []    
   for i in range(len(text)):
-    encoded_dict = tokenizer(text[i], max_length = 512, padding = 'max_length', return_attention_mask = True, truncation = True)
+    if y_str:
+      encoded_dict = tokenizer.encode_plus(text[i], y_str[i], max_length = 512, padding = 'max_length', return_attention_mask = True, truncation = True)
+    else:
+      encoded_dict = tokenizer.encode_plus(text[i], max_length = 512, padding = 'max_length', return_attention_mask = True, truncation = True)
     input_ids.append(encoded_dict.input_ids)
     attention_masks.append(encoded_dict.attention_mask)
     seg_ids.append(encoded_dict.token_type_ids)
     mask_pos.append(input_ids[i].index(tokenizer.mask_token_id))
-  
+      
   return input_ids, seg_ids, attention_masks, mask_pos
 
 
-def data_helper_bert(x_train_all, x_val_all, x_test_all, model_select):        
+def data_helper_bert(x_train_all,x_val_all,x_test_all,model_select):        
+    x_train, y_train, y_train_str = x_train_all[0],x_train_all[1],x_train_all[2]
+    x_val, y_val, y_val_str = x_val_all[0],x_val_all[1],x_val_all[2]
+    x_test, y_test = x_test_all[0],x_test_all[1]
     
-  x_train, y_train = x_train_all[0], x_train_all[1]
-  x_val, y_val = x_val_all[0], x_val_all[1]
-  x_test, y_test = x_test_all[0], x_test_all[1]
-  
-  if model_select == 'Bertweet':
-    tokenizer = BertweetTokenizer.from_pretrained("vinai/bertweet-base", normalization=True, mask_token='[MASK]')  
-  elif model_select == 'Bert':
-    tokenizer = BertTokenizer.from_pretrained("bert-base-uncased", do_lower_case=True, mask_token='[MASK]')   
+    if model_select == 'Bertweet':
+        tokenizer = BertweetTokenizer.from_pretrained("vinai/bertweet-base", normalization=True)
+    elif model_select == 'Bert':
+        tokenizer = BertTokenizer.from_pretrained("bert-base-uncased", do_lower_case=True, mask_token='[MASK]')
 
-  train_input_ids, train_seg_ids, train_atten_masks, train_mask_pos = convert_data_to_ids(tokenizer, x_train)
-  val_input_ids, val_seg_ids, val_atten_masks, val_mask_pos = convert_data_to_ids(tokenizer, x_val)  
-  test_input_ids, test_seg_ids, test_atten_masks, test_mask_pos = convert_data_to_ids(tokenizer, x_test)
-  
-  x_train_all = [train_input_ids, train_seg_ids, train_atten_masks, train_mask_pos, y_train]
-  x_val_all = [val_input_ids, val_seg_ids, val_atten_masks, val_mask_pos, y_val]
-  x_test_all = [test_input_ids, test_seg_ids, test_atten_masks, test_mask_pos, y_test]
-  
-  return x_train_all, x_val_all, x_test_all, tokenizer
+    x_train_input_ids, x_train_seg_ids, x_train_atten_masks, x_train_mask = convert_data_to_ids(tokenizer, x_train,y_train_str)
+    x_val_input_ids, x_val_seg_ids, x_val_atten_masks, x_val_mask = convert_data_to_ids(tokenizer, x_val, y_val_str)
+    x_test_input_ids, x_test_seg_ids, x_test_atten_masks, x_test_mask = convert_data_to_ids(tokenizer, x_test, None)
+    
+    x_train_all = [x_train_input_ids,x_train_seg_ids,x_train_atten_masks,y_train,x_train_mask]
+    x_val_all = [x_val_input_ids,x_val_seg_ids,x_val_atten_masks,y_val,x_val_mask]
+    x_test_all = [x_test_input_ids,x_test_seg_ids,x_test_atten_masks,y_test,x_test_mask]
+        
+    return x_train_all,x_val_all,x_test_all
 
 
-def data_loader(x_all, batch_size, model_select, mode, model_name, **kwargs):      
-  x_input_ids = torch.tensor(x_all[0], dtype=torch.long).cuda()
-  x_seg_ids = torch.tensor(x_all[1], dtype=torch.long).cuda()
-  x_atten_masks = torch.tensor(x_all[2], dtype=torch.long).cuda()
-  y = torch.tensor(x_all[4]).cuda()
-  x_mask = torch.tensor(x_all[3], dtype=torch.long).cuda()
+def data_loader(x_all, batch_size, model_select, mode, model_name, **kwargs):
+    
+    x_input_ids = torch.tensor(x_all[0], dtype=torch.long).cuda()
+    x_seg_ids = torch.tensor(x_all[1], dtype=torch.long).cuda()
+    x_atten_masks = torch.tensor(x_all[2], dtype=torch.long).cuda()
+    y = torch.tensor(x_all[3], dtype=torch.long).cuda()
+    x_mask = torch.tensor(x_all[4], dtype=torch.long).cuda()
 
-  if model_name == 'student' and mode == 'train':
-    y2 = torch.tensor(kwargs['y_train2'], dtype=torch.float).cuda()  # load teacher predictions
-    tensor_loader = TensorDataset(x_input_ids, x_seg_ids, x_atten_masks, x_mask, y, y2)
-  else:
-      tensor_loader = TensorDataset(x_input_ids, x_seg_ids, x_atten_masks, x_mask, y)
+    if model_name == 'student' and mode == 'train':
+        y2 = torch.tensor(kwargs['y_train2'], dtype=torch.float).cuda()  # load teacher predictions
+        tensor_loader = TensorDataset(x_input_ids,x_seg_ids,x_atten_masks,x_mask,y,y2)
+    else:
+        tensor_loader = TensorDataset(x_input_ids,x_seg_ids,x_atten_masks,x_mask,y)
 
-  if mode == 'train':
-    data_loader = DataLoader(tensor_loader, shuffle=True, batch_size=batch_size)
-    data_loader_distill = DataLoader(tensor_loader, shuffle=False, batch_size=batch_size)
-    return y, data_loader, data_loader_distill  
-  else:
-    data_loader = DataLoader(tensor_loader, shuffle=False, batch_size=batch_size)    
-    return y, data_loader
+    if mode == 'train':
+        data_loader = DataLoader(tensor_loader, shuffle=True, batch_size=batch_size)
+        data_loader_distill = DataLoader(tensor_loader, shuffle=False, batch_size=batch_size)
+        return y, data_loader, data_loader_distill
+    else:
+        data_loader = DataLoader(tensor_loader, shuffle=False, batch_size=batch_size)
+        return y, data_loader
 
 
-def sep_test_set(input_data):
-    data_list = [input_data[:1966], input_data[1966:12204], input_data[12204:]]
+def sep_test_set(input_data,dataset_name):
+    data_list = [input_data[:10238], input_data[10238:12204], input_data[12204:]]
     return data_list
