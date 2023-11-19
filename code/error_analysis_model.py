1e4d2950f394fb6f41bc479f9df803ca4675bbad
FatemehMomeni <50900356+FatemehMomeni@users.noreply.github.com>
2023-11-19 15:58:50 +0330

    Update error_analysis_model.py

--- code/error_analysis_model.py
+++ code/error_analysis_model.py
@@ -29,41 +29,38 @@
 
 import torch
 import torch.nn as nn
-from transformers import AutoModel, BertModel
+from transformers import AutoModel, BertModel, T5ForConditionalGeneration, T5Tokenizer
 
 
 class stance_classifier(nn.Module):
   def __init__(self,num_labels,model_select):
     super(stance_classifier, self).__init__()        
-    self.dropout = nn.Dropout(0.)
-    self.relu = nn.ReLU()        
+    # self.dropout = nn.Dropout(0.)
+    # self.relu = nn.ReLU()        
     if model_select == 'Bertweet':
       self.bert = AutoModel.from_pretrained("vinai/bertweet-base")
     elif model_select == 'Bert':
-      self.bert = BertModel.from_pretrained("bert-base-uncased")
-    self.bert.pooler = None
-    self.linear = nn.Linear(self.bert.config.hidden_size, self.bert.config.hidden_size)
-    self.out = nn.Linear(self.bert.config.hidden_size, num_labels)
+      # self.bert = BertModel.from_pretrained("bert-base-uncased")
+      self.bert = T5ForConditionalGeneration.from_pretrained("google/flan-t5-base")
+    # self.bert.pooler = None
+    # self.linear = nn.Linear(128,128) # self.bert.config.hidden_size)
+    # self.out = nn.Linear(128, num_labels)
     # self.labels = label_vectors
+    self.tokenizer = T5Tokenizer.from_pretrained("google/flan-t5-base")
 
 
-  def forward(self, x_input_ids, x_seg_ids, x_atten_masks, mask_pos):      
-    last_hidden = self.bert(input_ids=x_input_ids, attention_mask=x_atten_masks, token_type_ids=x_seg_ids)[0]
-    predictions = tuple()    
-    for i in range(len(x_input_ids)):
-      predicted_mask_token = last_hidden[i, mask_pos[i]]      
-      predictions += (predicted_mask_token,)
-    predictions = torch.stack(predictions).cuda()
-    #   a = (torch.dot(predicted_mask_token, self.labels[0]),)
-    #   b = (torch.dot(predicted_mask_token, self.labels[1]),)
-    #   c = (torch.dot(predicted_mask_token, self.labels[2]),)            
-    #   predictions += (torch.stack((a,b,c)).cuda(),)
-    # predictions_tensor = torch.stack(predictions).cuda()
-    # pred_reshape = self.mask_cls(prediction_tensor)
-    # last_hidden2 = self.bert(pred_reshape.long())
-    # cls = last_hidden2[0][:,0]
-    query = self.dropout(predictions)
-    linear = self.relu(self.linear(query))
-    out = self.out(linear)
+  def forward(self, input_ids, attention_mask, labels, train):     
+    if train:
+      last_hidden = self.bert(input_ids=input_ids, attention_mask=attention_mask, labels=labels)
+      output = last_hidden.loss
+    else:
+      last_hidden = self.bert.generate(input_ids=input_ids, attention_mask=attention_mask, do_sample=False) 
+      output = self.tokenizer.batch_decode(last_hidden, skip_special_tokens=True)[0]
     
-    return out
+    # cls = last_hidden[0][:,0]
+    # query = self.dropout(cls)
+    # linear = self.relu(self.linear(query))
+    # out = self.out(linear)    
+    # print(self.tokenizer.decode(last_hidden[0]))
+    
+    return output
