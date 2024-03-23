233c68dda2f93cc39c4d62f05c57fb2eddc9ee09
FatemehMomeni <50900356+FatemehMomeni@users.noreply.github.com>
2024-03-23 11:18:17 +0330

    Update modeling.py

--- proposed/train/modeling.py
+++ proposed/train/modeling.py
@@ -13,7 +13,8 @@ class StanceClassifier(nn.Module):
         self.bert.pooler = None
         self.linear = nn.Linear(num_labels['multi'] * 2, batch)
         self.out = nn.Linear(batch, 2)
-        self.pad = torch.tensor([-1 for _ in range(512)], dtype=torch.long).to('cuda')
+        self.pad = torch.zeros(batch, 512).to('cuda:0')
+        # self.pad = torch.tensor([-1 for _ in range(512)], dtype=torch.long).to('cuda')
         self.lv = torch.cat(label_vectors, 0)
         self.lv = torch.transpose(self.lv, 0, 1)
 
@@ -27,6 +28,7 @@ class StanceClassifier(nn.Module):
         else:
             last_hidden = self.bert(input_ids, attention_mask)[0]
             last_hidden2 = self.bert(input_ids2, attention_mask2)[0]
+            multi_index = list()
             similarity = torch.zeros(1, 3).cuda()
             similarity2 = torch.zeros(1, 3).cuda()
             for i in range(len(input_ids)):
@@ -36,6 +38,7 @@ class StanceClassifier(nn.Module):
                 similarity = torch.cat((similarity, torch.tensordot(h_mask, self.lv, dims=1)), 0)  # output size = 1*3
 
                 if not (input_ids2[i] == self.pad).all():
+                    multi_index.append(i)
                     h_mask2 = last_hidden2[i, mask_position2[i]]
                     h_mask2 = self.dropout(h_mask2)
                     h_mask2 = torch.unsqueeze(h_mask2, 0)
@@ -46,7 +49,8 @@ class StanceClassifier(nn.Module):
                 out2 = None
             else:
                 similarity2 = similarity2[1:]
-                context_vec = torch.cat((similarity, similarity2), dim=1)
+                similarity1 = similarity[multi_index]
+                context_vec = torch.cat((similarity1, similarity2), dim=1)
                 linear2 = self.relu(self.linear(context_vec))
                 out2 = self.out(linear2)
             
